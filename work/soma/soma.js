072d552167355066d95ff15590994f22f83c8a07
Nathan Harris <derjyn@gmail.com>
2014-10-09 10:06:38 -0700

    Minor Update
    * Cleaned things up
    * Updated style.min.css
    * Update Bones, Flesh, and Soma demos

--- work/soma/soma.js
+++ work/soma/soma.js
@@ -7,19 +7,30 @@ $(document).ready(function () {
 	// ParticleGround stuff
   $('#bg-fx').particleground({
   	minSpeedX: 0.2,
-  	maxSpeedX: 0.7,
+  	maxSpeedX: 0.5,
   	minSpeedY: 0.2,
-  	maxSpeedY: 0.7,
+  	maxSpeedY: 0.5,
   	directionX: 'center',		// center/up/down. center has the dots bouncing off edge of canvas
   	dirextionY: 'center',		// center/up/down. center has the dots bouncing off edge of canvas
   	density: 6000,					// particle density, one particle every n pixels (higher = less)
-  	dotColor: 'rgba(101,111,128,0.8)',
-  	lineColor: 'rgba(101,111,128,0.8)',
-  	particleRadius: 2,			// dot size
+  	dotColor: 'rgb(60,60,60)',
+  	lineColor: 'rgb(120,120,120)',
+  	particleRadius: 1,			// dot size
   	lineWidth: 0.3,
   	curvedLines: false,
   	proximity: 70,					// proximity before dots join
-  	parallax: true,
+  	parallax: false,
   	parallaxMultiplier: 25	// lower values make the parallax effect more extreme
 	});
+
+  $('#vid_header').videoBG({
+    mp4: 'assets/vid_header.mp4',
+    ogv: 'assets/vid_header.ogv',
+    webm: 'assets/vid_header.webm',
+    poster: 'assets/vid_header.png',
+    position: 'relative',
+    loop: true,
+    scale: false,
+    zIndex: -1
+  });
 });
